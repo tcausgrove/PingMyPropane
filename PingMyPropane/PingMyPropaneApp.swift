69f80829be6d88fa212a6dc13d35c8d62505a482
Tim Causgrove <tcausnm@gmail.com>
2023-03-11 10:12:42 -0600

    Initial Commit

---
+++ PingMyPropane/PingMyPropaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingMyPropaneApp.swift
+//  PingMyPropane
+//
+//  Created by Timothy Causgrove on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PingMyPropaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
