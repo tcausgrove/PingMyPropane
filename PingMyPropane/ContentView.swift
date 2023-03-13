54ff51962eb539a21cce2395d5875b47784cbb13
Tim Causgrove <tcausnm@gmail.com>
2023-03-12 19:13:37 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'
    Conflicts:
    	PingMyPropane/ContentView.swift

--- PingMyPropane/ContentView.swift
+++ PingMyPropane/ContentView.swift
@@ -6,17 +6,57 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+extension VerticalAlignment {
+    enum BottomOfWholeTank: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.bottom]
+        }
+    }
+
+    static let bottomOfWholeTank = VerticalAlignment(BottomOfWholeTank.self)
+}
 
 struct ContentView: View {
+    @State var indicatorPercentage: CGFloat = 50.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "square")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Spacer()
+                Section {
+                    Image(systemName: "record.circle")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Ping my propane")
+                }
+                .onTapGesture {
+                    indicatorPercentage = CGFloat.random(in: 0...100)
+                }
+                Spacer()
+                ZStack(alignment: .bottom) {
+                    Rectangle()
+                        .alignmentGuide(.bottomOfWholeTank) { d in d[VerticalAlignment.bottom]}
+                        .frame(width: 100, height: 200)
+                    Rectangle()
+                        .alignmentGuide(.bottomOfWholeTank) { d in d[VerticalAlignment.bottom]}
+                        .frame(width: 100, height: indicatorPercentage * 2)
+                        .foregroundColor(.red)
+                }
+                Spacer()
+                
+            }
         }
         .padding()
     }
+}
+
+func recordPing() {
+// Finish this later
+    
+//    let engine = AVAudioEngine()
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
